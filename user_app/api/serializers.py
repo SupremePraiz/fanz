def134fa9d0499e75a64016c5c8472902a4f8e0a
Supremepraiz <supremepraiz2@gmail.com>
2024-04-27 23:26:56 +0100

    added permissions

--- user_app/api/serializers.py
+++ user_app/api/serializers.py
@@ -3,30 +3,32 @@ from django.contrib.auth.models import User
 
 
 class RegistrationSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(style={"input_type":"password"},write_only=True)
-    password2 = serializers.CharField(style={"input_type":"password"},write_only=True)
+    password2 = serializers.CharField(style={"input_type":"password"}, write_only=True)
+    password = serializers.CharField(style={"input_type":"password"})
     
     class Meta:
         model = User
         fields = ["username", "email", "password", "password2"]
-        # extra_kwargs ={
-        #                "password":({"write_only": True}),
-        #                 }
+        extra_kwargs ={
+            "password":{"write_only": True},
+            
+        }
         
-        def save(self):
-            password = self.validated_data["password"]
-            password2 = self.validated_data["password2"]
+    def save(self):
+        password = self.validated_data["password"]
+        password2 = self.validated_data["password2"]
             
-            if password != password2:
-                raise serializers.ValidationError({"error":"Password1 and password2 must be the same"})
-            if User.objects.filter(email = self.validated_data["email"]).exists():
-                raise serializers.ValidationError({"error":"this email has already been used"})
-            if User.objects.filter(username=self.validated_data["username"]).exists():
-                raise serializers.ValidationError({"error":"this username has already been used"})
+        if password != password2:
+            raise serializers.ValidationError({"error":"password1 and password2 must be the same"})
+        if User.objects.filter(email=self.validated_data["email"]).exists():
+            raise serializers.ValidationError({"error":"this email has already been used"})
             
-               
-            account = User(email=self.validated_data["email"], username=self.validated_data["username"])
-            account.set_password(password)
-            account.save()
+        account =User(email=self.validated_data["email"], username=self.validated_data["username"])
+        account.set_password(password)
+        account.save()
             
-            return account
+        return account
+
+
+
+
